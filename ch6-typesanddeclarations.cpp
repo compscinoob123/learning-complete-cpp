2791dc9f6c0ed62f4340342ef5088fb883a01e3a
compscinoob123 <38302662+compscinoob123@users.noreply.github.com>
2018-04-29 00:40:02 -0500

    Create ch6-typesanddeclarations.cpp

---
+++ ch6-typesanddeclarations.cpp
@@ -0,0 +1,7 @@
+x = y+f(2)
+/* For this equation to actually work in c++, you must declare them. */
+
+float x; //x is a floating point varaible
+int y = 7; //y is an integer variable with the initial value 7
+float f(int); //f is declared to be a function that takes an 'int' as an argument, so it can be called given the integer 2
+
